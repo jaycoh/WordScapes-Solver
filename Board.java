f8981700fc97da24d6f561ee4687382e596434db
jaycoh <50001788+jaycoh@users.noreply.github.com>
2019-05-31 15:37:11 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Board {
     // Class for the board of a game. Constructor receives same information shown at start of a level: the letters,
     // minimum word size, and the layout of the solution. If no minimum word size given, assume 3.
-    
-    
+
+
     private Letters letters;
     private int minWordSize;
     private Layout layout;
@@ -26,12 +26,12 @@ public class Board {
 
     }
 
-    public ArrayList<String> solve() { 
+    public ArrayList<String> solve() {
         int maxWordLength = this.letters.getLength();
         this.words = this.letters.makeWords(this.minWordSize, maxWordLength);
         this.solution = new BoardSolution(this.layout, this.words);
         return this.solution.solve();
-    
+
 
 
     }
