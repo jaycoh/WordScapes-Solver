2e213628f7a4cccf0447e0bac1cfcf49e3bfddce
jaycoh <50001788+jaycoh@users.noreply.github.com>
2019-08-27 11:00:26 -0700

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,32 @@
+public class Tile {
+
+    private String letter;
+
+    public Tile() {
+        this.letter = null;
+    }
+
+    public Tile(String letter) {
+        this.letter = letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+
+    public void makeNull() {
+        this.letter = null;
+    }
+
+    public String getLetter() {
+        return this.letter;
+    }
+
+    public void removeLetter() {
+        this.letter = null;
+    }
+
+    public boolean hasLetter() {
+        return this.letter != null;
+    }
+}
