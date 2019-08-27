abea4155bdacf0363d743a99bd03bf3a232dd260
jaycoh <50001788+jaycoh@users.noreply.github.com>
2019-08-27 10:50:17 -0700

    Update BoardSolution.java

--- BoardSolution.java
+++ BoardSolution.java
@@ -1,98 +1,125 @@
 import java.util.ArrayList;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardSolution {
     // Class used to solve the board.
 
     private ArrayList<String> potentialWords;
-    private ArrayList<String> arraySolution;
     private Layout layout;
 
     public BoardSolution(Layout layout, ArrayList<String> potentialWords) {
         this.potentialWords = potentialWords;
-        this.arraySolution = null;
         this.layout = layout;
 
     }
 
     public void getPotentialWords() {
+
         System.out.println(potentialWords);
     }
 
     public boolean validWord(String word, Word placement) {
-        // Determines if a word is valid for a specific placement. Currently depends solely on length of word (will be
-        // updated).
+        // Determines if a word is valid for a specific placement. Depends on length of word and previously set tiles
+
+        if (correctLength(word, placement)) {
+                return matchesTiles(word, placement);
+        }
+        return false;
+    }
+
+    public boolean correctLength(String word, Word placement) {
+
         return (word.length() == placement.getWordLength());
 
     }
 
-    public ArrayList<String> solve() {
-        // Abstract function to solve the board.
-        ArrayList<String> startingBoard = new ArrayList<String>();
-        return solveBoard(startingBoard, this.layout.getWords());
-    }
+    public boolean matchesTiles(String word, Word placement) {
 
-    public ArrayList<String> solveBoard(ArrayList<String> currentSolution,
-                                        ArrayList <Word> remainingLayout) {
-        // Finds a solution to a given board
-        if (remainingLayout.isEmpty()) {
-            return currentSolution;
-        } else {
-            ArrayList<ArrayList<String>> nextBoards = nextBoard(currentSolution, remainingLayout);
-            return solveBoardList(nextBoards, removeFirst(remainingLayout));
+        int count = 0;
+
+        while (count < placement.getWordLength()) {
+            if (placement.getTile(count).getLetter() != null) {
+                if (!sameLetter(word, placement, count)) {
+                    return false;
+                }
+
+            }
+            count++;
         }
+        return true;
     }
 
-    public ArrayList<String> solveBoardList(ArrayList<ArrayList<String>> nextBoards, ArrayList<Word> remainingLayout) {
-        // Finds a solution from a list of boards
-        ArrayList<String> returnedBoard = new ArrayList<String>();
-        if (!(nextBoards.isEmpty())) {
 
-            for (ArrayList<String> board : nextBoards) {
-                ArrayList<String> check = solveBoard(board, remainingLayout);
-                if (!(check.isEmpty())) {
+
+    public boolean sameLetter(String word, Word placement, int index) {
+        return placement.getTile(index).getLetter().equals(word.charAt(index) + "");
+    }
+
+    public ArrayList<Word> solveBoard(ArrayList<Word> wordSpaces) {
+        if (isFinished(wordSpaces)) {
+            return wordSpaces;
+        }
+
+            ArrayList<String> containedWords = containedWords(wordSpaces);
+            Word wordToSolve = findNextWord(wordSpaces);
+            ArrayList<Tile> currentLetters = wordToSolve.getLetters();
+            ArrayList<String> potentialWords = validNextWords(wordToSolve, containedWords);
+
+            for (String potentialWord : potentialWords) {
+                wordToSolve.setWord(potentialWord);
+                ArrayList<Word> check = solveBoard(wordSpaces);
+                if (check != null) {
                     return check;
                 }
+                wordToSolve.setWord(currentLetters);
             }
-        }
-        // if list of boards is empty or none of them produce a non-empty solution, returns an empty array
-        return returnedBoard;
+            return null;
     }
 
-    public ArrayList<ArrayList<String>> nextBoard(ArrayList<String> currentSolution,
-                                                  ArrayList<Word> remainingLayout) {
-        // Creates a list of the next boards.
-        ArrayList<ArrayList<String>> nextBoards = new ArrayList<>();
+    public ArrayList<Word> solve() {
+        return solveBoard(layout.getWords());
+    }
+
+    public ArrayList<String> validNextWords(Word wordToSolve, ArrayList<String> usedWords) {
+        ArrayList<String> wordChoices = new ArrayList<>();
         for (String word : this.potentialWords) {
-            if (validWord(word, remainingLayout.get(0)) && !(currentSolution.contains(word))) {
-                ArrayList<String> newBoard = makeNextBoard(currentSolution, word);
-                nextBoards.add(newBoard);
+            if (!usedWords.contains(word) && validWord(word, wordToSolve)) {
+                wordChoices.add(word);
             }
         }
-        return nextBoards;
+        return wordChoices;
     }
 
-    public ArrayList<String> makeNextBoard(ArrayList<String> currentSolution, String word) {
-        //Creates the next board.
-        ArrayList<String> newBoard = new ArrayList<String>();
-        int count = 0;
-        while (count < currentSolution.size()) {
-            newBoard.add(currentSolution.get(count));
-            count++;
+
+
+    public Word findNextWord(ArrayList<Word> wordList) {
+        for (Word word : wordList) {
+            if (!word.isComplete()) {
+                return word;
+            }
         }
-        newBoard.add(word);
-        return newBoard;
+        return null;
     }
 
-    public ArrayList<Word> removeFirst(ArrayList<Word> currentLayout) {
-
-        //Removes the first Word from an ArrayList<Word>.
-
-        ArrayList<Word> nextLayout = new ArrayList<Word>();
-        int count = 1;
-        while (count < currentLayout.size()) {
-            nextLayout.add(currentLayout.get(count));
-            count++;
+    public ArrayList<String> containedWords(ArrayList<Word> listOfWord) {
+        ArrayList<String> containedWords = new ArrayList<String>();
+        for (Word word : listOfWord) {
+            if (word.isComplete()) {
+                containedWords.add(word.returnWordString());
+            }
         }
-        return nextLayout;
+        return containedWords;
+    }
+
+    public boolean isFinished(ArrayList<Word> wordList) {
+        for (Word word : wordList) {
+            if (!word.isComplete()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
